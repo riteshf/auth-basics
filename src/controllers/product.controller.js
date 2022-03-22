def9b15199aa0421ea57d1f186e2aef65730c73b
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-03-22 21:39:28 +0530

    poducts auth check added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const authCheck = require("../middlewares/auth.middleware");
 
 const Product = require("../models/product.model");
 
 const router = express.Router();
 
-router.post("", async (req, res) => {
+router.post("", authCheck, async (req, res) => {
   try {
-    const product = await Product.create(req.body);
+    console.log(req.user);
+    const product = await Product.create({
+      ...req.body,
+      user_id: req.user._id,
+    });
 
     return res.send(product);
   } catch (err) {
@@ -17,6 +22,16 @@ router.post("", async (req, res) => {
 router.get("", async (req, res) => {
   try {
     const products = await Product.find();
+
+    return res.send(products);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.get("/my", authCheck, async (req, res) => {
+  try {
+    const products = await Product.find({ user_id: req.user._id });
 
     return res.send(products);
   } catch (err) {
